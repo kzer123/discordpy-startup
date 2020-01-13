411a7aa6cc12393b5425e3221f3addabce65dac5
kzer123 <59829259+kzer123@users.noreply.github.com>
2020-01-13 23:07:05 +0900

    Update callbot.py

--- callbot.py
+++ callbot.py
@@ -17,4 +17,4 @@ async def on_voice_state_update(member, before, after):
             msg = f'{now:%m/%d-%H:%M} に {member.name} が {before.channel.name} から退出しました。'
             await alert_channel.send(msg) 
  
-client.run("NjY2MTcxNjY2MDU3MDAzMDE5.XhxxTg.YCIWppBN_J_chT9lc3espU8AdxA")
+client.run("NjY2MTcxNjY2MDU3MDAzMDE5.Xhx41g.NJawrc1Oyr1FyVSBMKATBqm968k")
